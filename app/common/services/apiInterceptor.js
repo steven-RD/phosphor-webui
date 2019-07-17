f973fc2a0396653d8eb38390011b5b08540056c6
judy <judy@osbzr.com>
2019-07-17 14:30:46 +0800

    Fix: timeout time.

--- app/common/services/apiInterceptor.js
+++ app/common/services/apiInterceptor.js
@@ -17,9 +17,10 @@ window.angular && (function(angular) {
         'request': function(config) {
           dataService.loading = true;
           // If caller has not defined a timeout, set to default of 20s
-          if (config.timeout == null) {
-            config.timeout = 20000;
-          }
+          // Judy annovation 20190717
+          //if (config.timeout == null) {
+            //config.timeout = 20000;
+          //}
           return config;
         },
         'response': function(response) {
