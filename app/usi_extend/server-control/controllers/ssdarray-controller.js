8ec1a01efddb13ee1f3e99e828af54803e532074
Steven <bao_gang_168@163.com>
2020-03-30 17:05:21 +0800

    add arrow loop

--- app/usi_extend/server-control/controllers/ssdarray-controller.js
+++ app/usi_extend/server-control/controllers/ssdarray-controller.js
@@ -60,6 +60,15 @@ window.angular && (function(angular) {
             console.log('equal');
 			
 			var lab = document.getElementById(ssdNum);
+			var windowEvent = window.event;               ///Get windowEvent
+			var mousePosition = getMousePos(windowEvent); ///Get mouse position
+			lab.style.left = mousePosition.x + 'px';
+			lab.style.top = mousePosition.y + 'px';
+			//lab.style.left = x + 'px';
+			//lab.style.top = y + 'px';
+			console.log(mousePosition.x + 'px');
+			console.log(mousePosition.y + 'px');
+				
 		    var description = ["SSD NO.", "Address", "Type", "Status", "Link Speed", "Link Width", 
 				"Link Status", "Inserted", "Partition ID", "Configure Width"];
             var ssd = [ssdNum, ssdInfo.SlotAddr, ssdInfo.Type, ssdInfo.Status, ssdInfo.LinkSpeed, 
@@ -79,7 +88,7 @@ window.angular && (function(angular) {
          }    
          tab+='</table>';
          lab.innerHTML=tab;
-		 lab.style.display="";
+		 lab.style.display="block";
         }
       });
     };
@@ -90,11 +99,20 @@ window.angular && (function(angular) {
             console.log(psInfo);
             console.log(psx);
             if(angular.equals(name, psx)){
-                console.log('equael');
+                console.log('equal');
                 console.log(name);
                 console.log(psInfo);
 				
 				var lab=document.getElementById(name);
+				var windowEvent = window.event;               ///Get windowEvent
+				var mousePosition = getMousePos(windowEvent); ///Get mouse position
+                lab.style.left = mousePosition.x + 'px';
+				lab.style.top = mousePosition.y + 'px';
+				//lab.style.left = x + 'px';
+				//lab.style.top = y + 'px';
+				console.log(mousePosition.x + 'px');
+				console.log(mousePosition.y + 'px');
+				
 				var tab='<table border=1 align="center">'
 			    tab+="<tr><td align='center' valian='middle' colspan='2'><b>PowerSupplya Information</b></td></tr>"
 				tab+='<tr>'
@@ -103,7 +121,7 @@ window.angular && (function(angular) {
 				tab+='</tr>'  
 				tab+='</table>';
 				lab.innerHTML=tab;
-				lab.style.display="";
+				lab.style.display="block";
 			}
         });
     };
@@ -119,6 +137,15 @@ window.angular && (function(angular) {
 				console.log('equal');
 				
 				var lab=document.getElementById(name);
+				var windowEvent = window.event;               ///Get windowEvent
+				var mousePosition = getMousePos(windowEvent); ///Get mouse position
+                lab.style.left = mousePosition.x + 'px';
+				lab.style.top = mousePosition.y + 'px';
+				//lab.style.left = x + 'px';
+				//lab.style.top = y + 'px';
+				console.log(mousePosition.x + 'px');
+				console.log(mousePosition.y + 'px');
+				
 				var description = ["Cable NO.", "Status", "Cable Type", "Slot Addr", "Partition ID",
 					"Link Status", "Link Active", "Link Width", "Physical Port", "UspDsp", "Present"];
 				var cable = [name, cableInfo.Status, cableInfo.CableType, cableInfo.SlotAddr, cableInfo.PartitionID, cableInfo.LinkStatus, 
@@ -140,7 +167,7 @@ window.angular && (function(angular) {
 			   }    
 			   tab+='</table>';
 			   lab.innerHTML=tab;
-			   lab.style.display="";
+			   lab.style.display="block";
 			}
 		});
 	};
@@ -172,6 +199,38 @@ window.angular && (function(angular) {
 		lab.style.display="";			
 		
 	};
+	
+	  //arrow loop
+	  var index = 0;
+      var imgLen = $(".img li").length; // Get the number of images
+      console.log(imgLen);
+      $scope.moveNext = function(arrow){
+          if(arrow == 'right'){
+            index++;
+            if (index == imgLen){
+                index = 0; // The first image
+            }
+            $(".img li").eq(index).stop().fadeIn(10).siblings().stop().fadeOut(10);
+          }else if(arrow == 'left'){
+            index--;
+            if (index == -1){
+                index = imgLen - 1; // The last image
+            }
+            $(".img li").eq(index).stop().fadeIn(10).siblings().stop().fadeOut(10);
+          }
+      };
+	
+	  // Get mouse relative position
+      function getMousePos(event) {
+          var scrollX = document.documentElement.scrollLeft || document.body.scrollLeft;
+          var scrollY = document.documentElement.scrollTop || document.body.scrollTop;
+		  //var x = event.pageX || event.clientX + scrollX;
+		  //var y = event.pageY || event.clientY + scrollY;
+		  var x = event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+		  var y = event.clientY+ document.body.scrollTop + document.documentElement.scrollTop;
+		  return { 'x': x, 'y': y };
+      }
+	
 	
 	
       $scope.loadSsdInfo = function(){
