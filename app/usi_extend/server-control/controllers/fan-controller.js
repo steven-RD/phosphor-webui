2d11e901f240941c296e83bd85ccb85901a9b574
Steven <bao_gang_168@163.com>
2020-04-30 09:50:02 +0800

    add fan page

--- app/usi_extend/server-control/controllers/fan-controller.js
+++ app/usi_extend/server-control/controllers/fan-controller.js
@@ -17,8 +17,19 @@ window.angular && (function(angular) {
     angular.module('app.serverControl').controller('fanController', [
     '$scope', '$window', 'UsiAPIUtils', 'APIUtils', 'dataService', 'toastService',
     function($scope, $window, UsiAPIUtils, APIUtils, dataService, toastService) {
-      $scope.loading = false;
-
+		$scope.loading = false;
+		
+		$scope.setFanSpeed() {
+			$scope.loading = true;
+			var data = {};
+			if ($scope.Fan1_INLET != '') {
+				data['Fan1_INLET'] = $scope.Fan1_INLET;
+            }
+			if ($scope.Fan1_OUTLET != '') {
+				data['Fan1_OUTLET'] = $scope.Fan1_OUTLET;
+            }
+			
+		};
 
 	
 
