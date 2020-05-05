a47f86aeb65e4976bd9ae68b26d69e11425ea6a3
Steven <bao_gang_168@163.com>
2020-05-05 15:19:37 +0800

    modify fan page

--- app/usi_extend/server-control/controllers/fan-controller.js
+++ app/usi_extend/server-control/controllers/fan-controller.js
@@ -19,19 +19,21 @@ window.angular && (function(angular) {
     function($scope, $window, UsiAPIUtils, APIUtils, dataService, toastService) {
 		$scope.loading = false;
 		
+		$scope.fanOwners = ['Fan1_INLET', 'Fan1_OUTLET', 'Fan2_INLET', 'Fan2_OUTLET', 
+							 'Fan3_INLET', 'Fan3_OUTLET', 'Fan4_INLET', 'Fan4_OUTLET',
+							 'Fan5_INLET', 'Fan5_OUTLET', 'Fan6_INLET', 'Fan6_OUTLET'];
+		
 		$scope.setFanSpeed = function() {
 			$scope.loading = true;
+			console.log($scope.fan.owner);
 			var data = {};
 			if ($scope.Fan1_INLET != '') {
-				data['Fan1_INLET'] = $scope.Fan1_INLET;
-            }
-			if ($scope.Fan1_OUTLET != '') {
-				data['Fan1_OUTLET'] = $scope.Fan1_OUTLET;
+				data[$scope.fan.owner] = $scope.Fanx;
             }
 			
 		};
 
-	
+		$scope.setFanSpeed();
 
       
     }
