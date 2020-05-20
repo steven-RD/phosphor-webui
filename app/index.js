552881106d53dd80b74ee352caaa8f87c0d90ed0
Steven <bao_gang_168@163.com>
2020-05-20 15:16:57 +0800

    debug

--- app/index.js
+++ app/index.js
@@ -101,7 +101,7 @@ import ssdarray_controller from './usi_extend/server-control/controllers/ssdarra
 import fan_controller from './usi_extend/server-control/controllers/fan-controller.js';
 import partition_controller from './usi_extend/server-control/controllers/partition-controller.js';
 import switch_firmware_controller from './usi_extend/configuration/controllers/switch-firmware-controller.js';
-import help_controller from './usi_extend/help/controllers/help-controller.js';
+//import help_controller from './usi_extend/help/controllers/help-controller.js';
 import usi_extend_server_control_index from './usi_extend/server-control/index.js';
 
 import users_index from './users/index.js';
