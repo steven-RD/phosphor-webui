862108622be9c43d4034e0788367ab9ab45aa543
Steven <bao_gang_168@163.com>
2020-06-01 13:55:12 +0800

    optimize partition2 table

--- app/usi_extend/server-control/controllers/partition-controller.js
+++ app/usi_extend/server-control/controllers/partition-controller.js
@@ -46,7 +46,7 @@ window.angular && (function(angular) {
 			    "Dspid-23": "bound to ssd-23.",
 			    "Dspid-24": "bound to ssd-24."
 			}
-		};
+		}; */
 		var Patopoinfo = {
 			"Partition(0)": {
 			    "Dspid-1": "bound to ssd-1.",
@@ -76,9 +76,9 @@ window.angular && (function(angular) {
 			    "Dspid-23": "bound to ssd-23.",
 			    "Dspid-24": "bound to ssd-24."
 			}
-		}; */
+		};
 		
-		var Patopoinfo = {
+		/* var Patopoinfo = {
 			"Partition(0)": {
 			    "Dspid-1": "bound to ssd-1.",
 			    "Dspid-2": "bound to ssd-2.",
@@ -111,7 +111,7 @@ window.angular && (function(angular) {
 			    "Dspid-23": "bound to ssd-23.",
 			    "Dspid-24": "bound to ssd-24."
 			} 
-		}; 
+		};  */
 
 		
 		$scope.loadParInfo = function() {
