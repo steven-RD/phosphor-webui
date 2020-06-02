e2d47b22962cda6ef27a5f8b101ada3cf54e2b76
Steven <bao_gang_168@163.com>
2020-06-02 10:39:30 +0800

    debug aad logo show

--- app/usi_extend/server-control/controllers/ssdarray-controller.js
+++ app/usi_extend/server-control/controllers/ssdarray-controller.js
@@ -72,9 +72,9 @@ window.angular && (function(angular) {
       ///ssd information
       $scope.SSD = function(num){
           //console.log(ssdMessage);
-			$scope.ssd1url = "../../icons/icon-ssd-good.svg";
+			$scope.ssd1url = "../../icons/icon-help.svg";
 			$scope.ssd1Flag = true;
-			$scope.ssd2url = "../../icons/icon-ssd-bad.svg";
+			$scope.ssd2url = "../../icons/icon-health.svg";
 			$scope.ssd2Flag =true;
 			console.log($scope.ssd2url);
       angular.forEach(ssdMessage, function(ssdInfo, ssdNum){
