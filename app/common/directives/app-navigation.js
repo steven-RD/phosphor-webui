7788984c9b65a0d4aae31033d42ee1047e951ed7
Steven <bao_gang_168@163.com>
2020-06-17 09:54:24 +0800

    add authory for fan control

--- app/common/directives/app-navigation.js
+++ app/common/directives/app-navigation.js
@@ -50,29 +50,30 @@ window.angular && (function(angular) {
                 $scope.navStyle = {'top': paddingTop + 'px'};
               });
 
-			  /** add by USI steven 20200616 start **/
-			  $scope.userName = '';
-			  $scope.getLogInUserName = function() {
+            /** add by USI steven 20200616 start **/
+            $scope.userName = '';
+            $scope.getLogInUserName = function() {
                 $scope.userName = userModel.getUserName();
-				console.log($scope.userName);
-              };
-			  $scope.loadUserInfo = function() {
-				var users = [];
+                console.log($scope.userName);
+            };
+            $scope.loadUserInfo = function() {
+                var users = [];
                 APIUtils.getAllUserAccounts().then(
                     function(res) {
-                      users = res;
-					  console.log(users);
-					  for(var i = 0; i < users.length; i++) {
-						  if($scope.userName = users[i].UserName){
-							  $scope.role = users[i].RoleId;
-							  console.log($scope.role);
-						  }
-					  }
+                        users = res;
+                        console.log(users);
+                        for(var i = 0; i < users.length; i++) {
+                            if($scope.userName = users[i].UserName){
+                                $scope.role = users[i].RoleId;
+                                console.log($scope.role);
+                            }
+                        }
                     },
                     function(error) {
                       console.log(JSON.stringify(error));
-                }),
-         };
+                    }
+               );
+           };
 
          $scope.getLogInUserName();
          $scope.loadUserInfo();
