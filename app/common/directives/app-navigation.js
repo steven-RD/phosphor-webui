ce971f29083f71a03649a161741e7485f9103207
Steven <bao_gang_168@163.com>
2020-06-29 11:07:01 +0800

    optimie page

--- app/common/directives/app-navigation.js
+++ app/common/directives/app-navigation.js
@@ -54,18 +54,17 @@ window.angular && (function(angular) {
             $scope.userName = '';
             $scope.getLogInUserName = function() {
                 $scope.userName = userModel.getUserName();
-                console.log($scope.userName);
+                //console.log($scope.userName);
             };
             $scope.loadUserInfo = function() {
                 var users = [];
                 APIUtils.getAllUserAccounts().then(
                     function(res) {
                         users = res;
-                        console.log(users);
                         for(var i = 0; i < users.length; i++) {
                             if($scope.userName == users[i].UserName){
                                 $scope.role = users[i].RoleId;
-                                console.log($scope.role);
+                                //console.log($scope.role);
                             }
                         }
                     },
